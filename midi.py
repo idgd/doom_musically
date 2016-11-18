6dc9058c6f3f7d2e34edf23cc259faa01b453261
idgd <isaac.dudney@icloud.com>
2016-11-17 20:19:37 -0800

    added classes, began test case skeleton

--- midi.py
+++ midi.py
@@ -1,4 +1,35 @@
 from music21 import *
 
-e1m1 = converter.parse("d_e1m1.mid")
-e1m1.show()
+class Song:
+   name = ''
+   tempo = 0.0
+   tracks = []
+   def __init__(self,name,tempo,tracks):
+      self.name = name
+      self.tempo = tempo
+      self.tracks = tracks
+
+class Track:
+   name = ''
+   notes = []
+   def __init__(self, name, notes):
+      self.name = name
+      self.notes = notes
+
+class MarkovNoteChain:
+   noteDict = {}
+
+newTracks = []
+
+e1m1 = converter.parse("mid/d_e1m1.mid")
+for addTrack in e1m1:
+    newTrack = []
+    for addNote in addTrack:
+        if type(addNote) == note.Note:
+           newTrack.append(note.Note(addNote.pitch))
+        if type(addNote) == note.Rest:
+           newTrack.append(addNote)
+
+    newTracks.append(newTrack)
+    newTrack = []
+
